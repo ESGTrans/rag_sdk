ac829cdb45fe58751bc7a31fd62cf6bcb484f724
A-baoYang <rubyyang0615@gmail.com>
2024-02-14 09:50:12 +0000

    formatting

--- rag_sdk/utils/api_utils.py
+++ rag_sdk/utils/api_utils.py
@@ -19,31 +19,31 @@ class RequestSender:
             self.headers = {
                 "User-Agent": self.ua.google,
                 "Accept": "application/json",
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             }
-    
+
     def send_request(
-        self, method: str, url: str, params: dict = None, data: dict = None, 
-        **kwargs
+        self, method: str, url: str, params: dict = None, data: dict = None, **kwargs
     ):
         response = request.request(
-            method, url, 
-            headers=self.headers, params=params, data=data,
-            **kwargs
+            method, url, headers=self.headers, params=params, data=data, **kwargs
         )
         return response.json()
 
     def send_streaming_request(
-        self, method: str, url: str, params: dict = None, data: dict = None, 
-        **kwargs
+        self, method: str, url: str, params: dict = None, data: dict = None, **kwargs
     ):
         try_num = 0
         while try_num <= self.default_retries:
             try:
 
                 with httpx.stream(
-                    method, url, headers=self.headers, params=params,
-                    json=data, timeout=self.default_timeout
+                    method,
+                    url,
+                    headers=self.headers,
+                    params=params,
+                    json=data,
+                    timeout=self.default_timeout,
                 ) as r:
                     for text in r.iter_text():
                         yield text
