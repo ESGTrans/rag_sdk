ef2c03d6e63739d9827651970a7ba53c88936c01
A-baoYang <rubyyang0615@gmail.com>
2024-02-14 09:22:23 +0000

    [feature] Add core module

---
+++ rag_sdk/core/base.py
@@ -0,0 +1,54 @@
+import os
+from rag_sdk.utils.api_utils import RequestSender
+
+
+class RAGCore:
+    ENDPOINT = 'https://rag.laplace-ai.com'
+    def __init__(
+        self, llm_model: str = "gpt-3.5-turbo", 
+        embedding_model: str = "shibing624/text2vec-base-chinese", 
+        vector_database_uri: str = None, collection_list: list = []
+    ):
+        self.llm_model = llm_model
+        self.embedding_model = embedding_model
+        self.vector_database_uri = vector_database_uri
+        self.collection_list = collection_list
+
+        self.request_sender = RequestSender()
+
+    def show_available_llm_models(self):
+        pass
+
+    def show_available_embedding_models(self):
+        pass
+    
+    def show_available_collections(self):
+        assert self.vector_database_uri is not None, "vector_database_uri is not provided!"
+        pass
+
+    def search(
+        self, query: str, top_k: int = 5, filters: dict = {},
+        verbose: bool = False, **kwargs
+    ):
+        pass
+
+    def chat(
+        self, query: str, top_k: int = 5, filters: dict = {},
+        verbose: bool = False, **kwargs
+    ):
+        uri = f"{self.ENDPOINT}/rag/chat"
+        data = {
+            "llm_model": self.llm_model,
+            "embedding_model": self.embedding_model,
+            "vector_database_uri": self.vector_database_uri,
+            "collection_list": self.collection_list,
+            "query": query,
+            "custom_topk": top_k,
+            "query_filters": filters,
+            "verbose": verbose,
+            **kwargs
+        }
+        response = self.request_sender.send_streaming_request(
+            "POST", uri, data=data
+        )
+        return response
---
+++ rag_sdk/utils/api_utils.py
@@ -0,0 +1,56 @@
+import httpx
+import requests
+from fake_useragent import UserAgent
+
+
+class RequestSender:
+
+    def __init__(self, headers: dict = None):
+        self.ua = UserAgent()
+        self._set_default_headers(headers)
+        self.default_retries = 3
+        self.default_timeout = 90
+        self.default_wait_seconds = 0.5
+
+    def _set_default_headers(self, headers: dict = None):
+        if headers:
+            self.headers = headers
+        else:
+            self.headers = {
+                "User-Agent": self.ua.google,
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+    
+    def send_request(
+        self, method: str, url: str, params: dict = None, data: dict = None, 
+        **kwargs
+    ):
+        response = request.request(
+            method, url, 
+            headers=self.headers, params=params, data=data,
+            **kwargs
+        )
+        return response.json()
+
+    def send_streaming_request(
+        self, method: str, url: str, params: dict = None, data: dict = None, 
+        **kwargs
+    ):
+        try_num = 0
+        while try_num <= self.default_retries:
+            try:
+
+                with httpx.stream(
+                    method, url, headers=self.headers, params=params
+                    json=data,
+                    timeout=self.default_timeout
+                ) as r:
+                    for text in r.iter_text():
+                        yield text
+                break
+
+            except Exception as ex:
+                print(f"Invalid chunk encoding {str(ex)}")
+                sleep(self.default_wait_seconds)
+                try_num += 1
